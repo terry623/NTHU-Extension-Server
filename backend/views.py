166ebd9219cf4b7af23b49263ae424832977120a
terry623 <terry92516@gmail.com>
2018-04-29 18:27:27 +0800

    add come

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, render_template, jsonify
-from .models import User,Course
+from .models import User, Course
+from flask_cors import CORS
 from random import *
 app = Flask(__name__)
+cors = CORS(app, resources={"/api/*": {"origins": "*"}})
+
 
 @app.route('/api/register', methods=['POST'])
 def register():
@@ -24,9 +27,37 @@ def loadCourseData():
     courseID = data['courseID']
 
     try:
-        response = Course(courseID).loadCourseData();
+        response = Course(courseID).loadCourseData()
     except:
         print("Load Course Data Error!")
+    return jsonify(response)
+
+
+@app.route('/api/calculateUserGrade', methods=['POST'])
+def calculateUserGrade():
+    # data = request.get_json()
+    print(request.form['stu_no'])
+    print(request.form['userGrade'])
+
+    try:
+        # response = Course(courseID).loadCourseData()
+        response = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+    except:
+        print("Calculate User Grade Error!")
+    return jsonify(response)
+
+
+@app.route('/api/collectGradeDistribution', methods=['POST'])
+def collectGradeDistribution():
+    # data = request.get_json()
+    print(request.form['course_no'])
+    print(request.form['distribution'])
+
+    try:
+        # response = Course(courseID).loadCourseData()
+        response = {'a': 6, 'b': 7, 'c': 8, 'd': 9, 'e': 10}
+    except:
+        print("Collect Grade Distribution Error!")
     return jsonify(response)
 
 
