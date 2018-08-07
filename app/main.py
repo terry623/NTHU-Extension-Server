0d5239e5a837e8f184e13f326babc7ba157709f1
terry623 <terry92516@gmail.com>
2018-08-07 23:17:50 +0800

    change dockerfile but no test

--- app/main.py
+++ app/main.py
@@ -130,4 +130,4 @@ def catch_all(path):
 
 if __name__ == "__main__":
     # Only for debugging while developing
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=True, port=8080)
