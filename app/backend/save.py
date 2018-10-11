a20d6f756749aec82c584d504824cbbe86526db6
terry623 <terry92516@gmail.com>
2018-10-11 17:27:43 +0800

    comment save to db

--- app/backend/save.py
+++ app/backend/save.py
@@ -1,25 +1,27 @@
-from pymongo import MongoClient
-import os
-account = os.environ["MONGO_ROLE_ACCOUNT"]
-password = os.environ["MONGO_ROLE_PASSWORD"]
-client = MongoClient('mongodb+srv://' + account + ':' + password +
-                     '@cluster0-hptar.gcp.mongodb.net/test?retryWrites=true')
+# from pymongo import MongoClient
+# import os
+# account = os.environ["MONGO_ROLE_ACCOUNT"]
+# password = os.environ["MONGO_ROLE_PASSWORD"]
+# client = MongoClient('mongodb+srv://' + account + ':' + password +
+#                      '@cluster0-hptar.gcp.mongodb.net/test?retryWrites=true')
 
-db = client['nthuCourse_database']
-userGrades = db['userGrade_collection']
-keywords = db['keyword_collection']
+# db = client['nthuCourse_database']
+# userGrades = db['userGrade_collection']
+# keywords = db['keyword_collection']
 
 
 def toSaveUserGrade(userGrade):
-    resultID = userGrades.insert_one({'userGrade': userGrade}).inserted_id
-    response = {'message': 'Save Grade Success!'}
+    # resultID = userGrades.insert_one({'userGrade': userGrade}).inserted_id
+    # response = {'message': 'Save Grade Success!'}
 
-    return response
+    # return response
+    return
 
 
 def toSaveKeyword(search_topic, keyword, other_keyword):
-    resultID = keywords.insert_one(
-        {'topic': search_topic, 'keyword': keyword, 'other_keyword': other_keyword}).inserted_id
-    response = {'message': 'Save Keyword Success!'}
+    # resultID = keywords.insert_one(
+    #     {'topic': search_topic, 'keyword': keyword, 'other_keyword': other_keyword}).inserted_id
+    # response = {'message': 'Save Keyword Success!'}
 
-    return response
+    # return response
+    return
